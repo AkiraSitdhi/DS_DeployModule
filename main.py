037f55913141210ca89148b62ac465d2b668a8ef
Akira Sitdhikariyawat <akira@192.168.68.102>
2023-05-19 07:34:49 +0700

    refactor

--- main.py
+++ main.py
@@ -22,21 +22,6 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.4303, 0.4301, 0.4139], std=[0.2186, 0.2140, 0.2205])
 ])
 
-def predictOutput(url_string : str):
-    urll = str(url_string)
-    try:
-        with urllib.request.urlopen(urll) as f:
-            image = Image.open(f)
-        tensor_image = transform(image)
-        tensor_image = tensor_image.numpy().astype(np.float32).reshape((1, 3, 224, 224))
-        prediction = model.run([], {'input': tensor_image})[0]
-        prediction = np.vectorize(thresholding_lambda)(prediction)[0].tolist()
-        return prediction
-    except Exception as e:
-        print('DEBUGGGGGGGGGGGGGGGGGGGG',str(e))
-        return None
-    
-
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
@@ -55,7 +40,7 @@ def index():
             prediction = np.vectorize(thresholding_lambda)(prediction)[0].tolist()
             return jsonify({"prediction":prediction})
         except Exception as e:
-            print('DEBUGGGGGGGGG')
+            # print('DEBUGGGGGGGGG')
             return jsonify({"error":str(e)})
 
     return "OK"
